1228440e5938c0c80b20302a0bfab6190cc98c05
ShaneDorin <shane.dorin@edu.devinci.fr>
2026-03-03 22:44:12 +0100

    [REMOVE] clean up unwanted comments and code in GameTest
    So much nicer now !

--- DAU_2025_Programming_Project_Template/DAU_2025_Programming_Project_Template/GameTest/GameTest.cpp
+++ DAU_2025_Programming_Project_Template/DAU_2025_Programming_Project_Template/GameTest/GameTest.cpp
@@ -12,45 +12,18 @@
 #include "Player.h"
 #include "EventManager.h"
 #include "InputManager.h"
-//------------------------------------------------------------------------
-// Eample data....
-//------------------------------------------------------------------------
-CSimpleSprite* testSprite;
-enum
-{
-	ANIM_FORWARDS,
-	ANIM_BACKWARDS,
-	ANIM_LEFT,
-	ANIM_RIGHT,
-};
+
 //------------------------------------------------------------------------
 // My data
 CSimpleSprite* placeHolderImage;
 sEventManager* event_manager; // singleton
 Player* player;
 
-// My input data
-float verticalInput;
-float horizontalInput;
-
 //------------------------------------------------------------------------
 // Called before first update. Do any initial setup here.
 //------------------------------------------------------------------------
 void Init()
 {
-	//------------------------------------------------------------------------
-	// Example Sprite Code....
-	//testSprite = App::CreateSprite(".\\TestData\\Test.bmp", 8, 4);
-	//testSprite = App::CreateSprite(".\\TestData\\PlaceHolderImage.png", 1, 1);
-	//testSprite->SetPosition(400.0f, 400.0f);
-	//float speed = 1.0f / 15.0f;
-	//testSprite->CreateAnimation(ANIM_BACKWARDS, speed, { 0,1,2,3,4,5,6,7 });
-	//testSprite->CreateAnimation(ANIM_LEFT, speed, { 8,9,10,11,12,13,14,15 });
-	//testSprite->CreateAnimation(ANIM_RIGHT, speed, { 16,17,18,19,20,21,22,23 });
-	//testSprite->CreateAnimation(ANIM_FORWARDS, speed, { 24,25,26,27,28,29,30,31 });
-	//testSprite->SetScale(1.0f);
-	//------------------------------------------------------------------------
-
 	// My sprite code
 	placeHolderImage = App::CreateSprite(".\\TestData\\PlaceHolderImage.png", 1, 1);
 	placeHolderImage->SetScale(1.0f);
@@ -61,7 +34,7 @@ void Init()
 	// My player code
 	player = new Player(400.0f, 400.0f, placeHolderImage);
 
-	//------------------------------------------------------------------------
+	//TO DO: innit all scene elements here. 
 }
 
 //------------------------------------------------------------------------
@@ -70,73 +43,8 @@ void Init()
 //------------------------------------------------------------------------
 void Update(float deltaTime)
 {
-	//------------------------------------------------------------------------
-	// Example Sprite Code....
-	//testSprite->Update(deltaTime);
-	//if (App::GetController().GetLeftThumbStickX() > 0.5f)
-	//{
-	//	//testSprite->SetAnimation(ANIM_RIGHT);
-	//	float x, y;
-	//	testSprite->GetPosition(x, y);
-	//	x += 1.0f;
-	//	testSprite->SetPosition(x, y);
-	//}
-	//if (App::GetController().GetLeftThumbStickX() < -0.5f)
-	//{
-	//	//testSprite->SetAnimation(ANIM_LEFT);
-	//	float x, y;
-	//	testSprite->GetPosition(x, y);
-	//	x -= 1.0f;
-	//	testSprite->SetPosition(x, y);
-	//}
- //   if (App::GetController().GetLeftThumbStickY() > 0.5f)
- //   {
- //       //testSprite->SetAnimation(ANIM_FORWARDS);
- //       float x, y;
- //       testSprite->GetPosition(x, y);
- //       y += 1.0f;
- //       testSprite->SetPosition(x, y);
- //   }
-	//if (App::GetController().GetLeftThumbStickY() < -0.5f)
-	//{
-	//	//testSprite->SetAnimation(ANIM_BACKWARDS);
-	//	float x, y;
-	//	testSprite->GetPosition(x, y);
-	//	y -= 1.0f;
-	//	testSprite->SetPosition(x, y);
-	//}
-	//if (App::GetController().CheckButton(XINPUT_GAMEPAD_DPAD_UP, false))
-	//{
-	//	testSprite->SetScale(testSprite->GetScale() + 0.1f);
-	//}
-	//if (App::GetController().CheckButton(XINPUT_GAMEPAD_DPAD_DOWN, false))
-	//{
-	//	testSprite->SetScale(testSprite->GetScale() - 0.1f);
-	//}
-	//if (App::GetController().CheckButton(XINPUT_GAMEPAD_DPAD_LEFT, false))
-	//{
-	//	testSprite->SetAngle(testSprite->GetAngle() + 0.1f);
-	//}
-	//if (App::GetController().CheckButton(XINPUT_GAMEPAD_DPAD_RIGHT, false))
-	//{
-	//	testSprite->SetAngle(testSprite->GetAngle() - 0.1f);
-	//}
-	//if (App::GetController().CheckButton(XINPUT_GAMEPAD_A, true))
-	//{
-	//	//testSprite->SetAnimation(-1);
-	//}
-	//------------------------------------------------------------------------
-	// My sprite code
 
 	InputManager::processUserInput();
-
-	// this does not work because this is a Windows app and not a Consol app. what is the difference ? 
-	// Should I add a console to my project ? I tried turning my project into a console app but that caused errors. 
-	//std::cout << "helllo world" << std::endl;
-
-	// this alowse me to 'print' into the output window but it is not very convenient. Printing floats with it is a bit complicated. 
-	//OutputDebugStringA("helllo world\n");
-
 
 	// apply input data
 	player->updateVelocity();
@@ -144,13 +52,10 @@ void Update(float deltaTime)
 	// update player position
 	player->updatePosition(deltaTime);
 
-	//------------------------------------------------------------------------
-	// Sample Sound.
-	//------------------------------------------------------------------------
-	if (App::GetController().CheckButton(XINPUT_GAMEPAD_B, true))
-	{
-		App::PlaySound(".\\TestData\\Test.wav");
-	}
+	/* 
+	Should I combine these two "player->" functions into one player->tick(deltaTime) function, 
+	or is it best to keep UPDATE LOGIC and UPDATE POSITION seperate ?
+	*/
 }
 
 //------------------------------------------------------------------------
@@ -159,49 +64,19 @@ void Update(float deltaTime)
 //------------------------------------------------------------------------
 void Render()
 {
-	//------------------------------------------------------------------------
-	// Example Sprite Code....
-	//testSprite->Draw();
 
 	player->draw();
-	//------------------------------------------------------------------------
 
-	//------------------------------------------------------------------------
-	// Example Text.
-	//------------------------------------------------------------------------
-	//App::Print(100, 100, "Sample Text");
-
-	//------------------------------------------------------------------------
-	// Example Line Drawing.
-	//------------------------------------------------------------------------
-	//static float a = 0.0f;
-	//float r = 1.0f;
-	//float g = 1.0f;
-	//float b = 1.0f;
-	//a += 0.1f;
-	//for (int i = 0; i < 20; i++)
-	//{
-
-	//	float sx = 200 + sinf(a + i * 0.1f)*60.0f;
-	//	float sy = 200 + cosf(a + i * 0.1f)*60.0f;
-	//	float ex = 700 - sinf(a + i * 0.1f)*60.0f;
-	//	float ey = 700 - cosf(a + i * 0.1f)*60.0f;
-	//	g = (float)i / 20.0f;
-	//	b = (float)i / 20.0f;
-	//	App::DrawLine(sx, sy, ex, ey,r,g,b);
-	//}
 }
+
 //------------------------------------------------------------------------
 // Add your shutdown code here. Called when the APP_QUIT_KEY is pressed.
 // Just before the app exits.
 //------------------------------------------------------------------------
 void Shutdown()
 {
-	//------------------------------------------------------------------------
-	// Example Sprite Code....
-	delete testSprite;
-	//------------------------------------------------------------------------
 
 	delete player; // player MUST be destroyed befor eventManager to satisfy assert(). 
 	sEventManager::destroyInstance();
+
 }
