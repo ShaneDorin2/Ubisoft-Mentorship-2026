635c6a5e204e8faada240b5c2482121f8edee434
ShaneDorin <shane.dorin@edu.devinci.fr>
2026-03-09 20:19:34 +0100

    [ADD] boid Alignment + organization of updateBoidLogic()

--- DAU_2025_Programming_Project_Template/DAU_2025_Programming_Project_Template/GameTest/BoidFlock.cpp
+++ DAU_2025_Programming_Project_Template/DAU_2025_Programming_Project_Template/GameTest/BoidFlock.cpp
@@ -16,37 +16,68 @@ BoidFlock::~BoidFlock()
 void BoidFlock::updateBoidLogic(float delta_time)
 {
 	// TODO put this data somewhere else
-	float protectedDistance = 100;
-	float visibleDistance = 50;
-	float avoidWeight = 1;
+	float protected_distance = 150;
+	float visible_distance = 200;
+	float avoid_weight = 0.001;
+	float alignment_weight = 0.1;
 
 
-	float close_dy = 0;
-	float close_dx = 0;
+	float close_dy;
+	float close_dx;
+
+	float x_vel_avg;
+	float y_vel_avg;
+	float num_of_boid_neibords;
 
 	for (Boid* boid : boids) {
 
-		// Separation
+		float new_x_vel = boid->getXVelocity();
+		float new_y_vel = boid->getYVelocity();
 
+		// Separation
 		close_dy = 0;
 		close_dx = 0;
+
+		// Alignment
+		x_vel_avg = 0;
+		y_vel_avg = 0;
+		num_of_boid_neibords = 0;
 
 		for (Boid* other_boid : boids) {
 			if (boid == other_boid) continue;
 
-			if (protectedDistance >= std::hypotf(
+			// Separation
+			if (protected_distance >= std::hypotf(
 				boid->getX() - other_boid->getX(), 
 				boid->getY() - other_boid->getY())
 				) {
 				close_dx += boid->getX() - other_boid->getX();
 				close_dy += boid->getY() - other_boid->getY();
 			}
+			// Alignment
+			else if (visible_distance >= std::hypotf(
+				boid->getX() - other_boid->getX(),
+				boid->getY() - other_boid->getY())
+				) {
+				x_vel_avg += other_boid->getXVelocity();
+				y_vel_avg += other_boid->getYVelocity();
+				num_of_boid_neibords++;
+			}
 		}
 
-		float new_x_vel = boid->getXVelocity() + close_dx * avoidWeight;
-		float new_y_vel = boid->getYVelocity() + close_dy * avoidWeight;
-		boid->updateVelocity(new_x_vel, new_y_vel);
+		// separation
+		new_x_vel += close_dx * avoid_weight;
+		new_y_vel += close_dy * avoid_weight;
 		
+		// alignment
+		if (num_of_boid_neibords != 0) {
+			x_vel_avg = x_vel_avg / num_of_boid_neibords;
+			y_vel_avg = y_vel_avg / num_of_boid_neibords;
+			new_x_vel += (x_vel_avg - new_x_vel) * alignment_weight;
+			new_y_vel += (y_vel_avg - new_y_vel) * alignment_weight;
+		}
+
+		boid->updateVelocity(new_x_vel, new_y_vel);
 	}
 
 	for (Boid* boid : boids) {
